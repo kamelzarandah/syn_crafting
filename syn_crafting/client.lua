f6a53311bb92a3b27916657d7ea419aa6fa7cae0
Blue <62485293+kamelzarandah@users.noreply.github.com>
2020-11-29 06:50:00 -0800

    Update client.lua

--- syn_crafting/client.lua
+++ syn_crafting/client.lua
@@ -194,3 +194,24 @@ AddEventHandler("syn:crafting", function()
         keyopen = false
         iscrafting = false
 end)
+
+RegisterNetEvent('syn:campfire')
+AddEventHandler('syn:campfire', function()
+
+    if campfire ~= 0 then
+        SetEntityAsMissionEntity(campfire)
+        DeleteObject(campfire)
+        campfire = 0
+    end
+    local playerPed = PlayerPedId()
+    TaskStartScenarioInPlace(playerPed, GetHashKey('WORLD_HUMAN_CROUCH_INSPECT'), 30000, true, false, false, false)
+    exports['progressBars']:startUI(30000, "You're placing a campfire...")
+    Citizen.Wait(30000)
+    ClearPedTasksImmediately(PlayerPedId())
+    local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 2.0, -1.55))
+    local prop = CreateObject(GetHashKey("p_campfire02x"), x, y, z, true, false, true)
+    SetEntityHeading(prop, GetEntityHeading(PlayerPedId()))
+    PlaceObjectOnGroundProperly(prop)
+    campfire = prop
+
+end)
